1c5c0597ae3cde0936e0625ad63c2cf76e91b1f9
2004yyj <2004yyj@naver.com>
2022-08-09 18:44:44 +0900

    [ADD] OrderDao, RecentlyDao Method

--- data/src/main/java/com/woowahan/ordering/data/local/db/AppDatabase.kt
+++ data/src/main/java/com/woowahan/ordering/data/local/db/AppDatabase.kt
@@ -6,6 +6,8 @@ import com.woowahan.ordering.data.entity.CartEntity
 import com.woowahan.ordering.data.entity.OrderEntity
 import com.woowahan.ordering.data.entity.RecentlyEntity
 import com.woowahan.ordering.data.local.dao.CartDao
+import com.woowahan.ordering.data.local.dao.OrderDao
+import com.woowahan.ordering.data.local.dao.RecentlyDao
 
 @Database(
     entities = [
@@ -17,4 +19,6 @@ import com.woowahan.ordering.data.local.dao.CartDao
 )
 abstract class AppDatabase: RoomDatabase() {
     abstract fun cartDao(): CartDao
+    abstract fun orderDao(): OrderDao
+    abstract fun recentlyDao(): RecentlyDao
 }
