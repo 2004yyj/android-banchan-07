f814161be4641b7e75f63fdeaed8f589647f2778
Jihye Joo <oreocube13@gmail.com>
2022-08-10 13:32:17 +0900

    [FIX] GET 요청 시 Path 어노테이션 추가

--- data/src/main/java/com/woowahan/ordering/data/remote/service/FoodService.kt
+++ data/src/main/java/com/woowahan/ordering/data/remote/service/FoodService.kt
@@ -3,20 +3,23 @@ package com.woowahan.ordering.data.remote.service
 import com.woowahan.ordering.data.entity.*
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FoodService {
     @GET("best")
-    fun getBestList() : Call<ListResponse<BestEntity>>
+    fun getBestList(): Call<ListResponse<BestEntity>>
 
     @GET("main")
-    fun getMainList() : Call<ListResponse<FoodEntity>>
+    fun getMainList(): Call<ListResponse<FoodEntity>>
 
     @GET("soup")
-    fun getSoupList() : Call<ListResponse<FoodEntity>>
+    fun getSoupList(): Call<ListResponse<FoodEntity>>
 
     @GET("side")
-    fun getSideList() : Call<ListResponse<FoodEntity>>
+    fun getSideList(): Call<ListResponse<FoodEntity>>
 
     @GET("detail/{hash}")
-    fun getFoodDetail(hash: String) : Call<FoodDetailResponse>
+    fun getFoodDetail(
+        @Path("hash") hash: String
+    ): Call<FoodDetailResponse>
 }
