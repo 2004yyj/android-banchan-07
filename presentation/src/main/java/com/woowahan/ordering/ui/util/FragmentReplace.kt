c1dbd50ee6260c69602d0bb47eb42ab06de82fbf
2004yyj <2004yyj@naver.com>
2022-08-10 15:44:16 +0900

    [ADD] FragmentReplace

---
+++ presentation/src/main/java/com/woowahan/ordering/ui/util/FragmentReplace.kt
@@ -0,0 +1,39 @@
+package com.woowahan.ordering.ui.util
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import java.util.function.Function
+
+fun <T: Fragment> FragmentManager.add(
+    fragmentClass: Class<T>,
+    containerViewId: Int,
+    tag: String,
+    arguments: Bundle = Bundle()
+) {
+    beginTransaction().apply {
+        val constructor = fragmentClass.getConstructor(Function::class.java)
+        val fragment: Fragment = constructor.newInstance()
+        fragment.arguments = arguments
+        add(containerViewId, fragment, tag)
+        commit()
+    }
+}
+
+fun <T: Fragment> FragmentManager.replace(
+    fragmentClass: Class<T>,
+    containerViewId: Int,
+    tag: String,
+    arguments: Bundle = Bundle()
+) {
+    beginTransaction().apply {
+        val constructor = fragmentClass.getConstructor()
+        val fragment: Fragment = findFragmentByTag(tag) ?: constructor.newInstance()
+        fragment.arguments = arguments
+        replace(containerViewId, fragment, tag)
+        if (findFragmentByTag(tag) == null) {
+            addToBackStack(tag)
+        }
+        commit()
+    }
+}
