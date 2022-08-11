7ca84a5cbdbf5a4c4c326ebe2e024c59230b05bd
2004yyj <2004yyj@naver.com>
2022-08-11 13:18:20 +0900

    [ADD] Header Adapter

---
+++ presentation/src/main/java/com/woowahan/ordering/ui/adapter/home/HeaderAdapter.kt
@@ -0,0 +1,31 @@
+package com.woowahan.ordering.ui.adapter.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.woowahan.ordering.databinding.ItemHeaderBinding
+import com.woowahan.ordering.ui.adapter.viewholder.ItemFoodViewHolder
+
+class HeaderAdapter(
+    private val title: String,
+    private val chip: String
+): RecyclerView.Adapter<HeaderAdapter.HeaderViewHolder>() {
+    inner class HeaderViewHolder(
+        private val binding: ItemHeaderBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind() = with(binding) {
+            tvChip.text = chip
+            tvTitle.text = title
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeaderViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemHeaderBinding.inflate(inflater, parent, false)
+        return HeaderViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HeaderViewHolder, position: Int) {
+        holder.bind()
+    }
+    override fun getItemCount(): Int = 1
+}
