a616ca142ab8afee26d3e34f268b0d164e5cfacd
2004yyj <2004yyj@naver.com>
2022-08-11 13:18:55 +0900

    [ADD] Grid Adapter

---
+++ presentation/src/main/java/com/woowahan/ordering/ui/adapter/home/FoodGridAdapter.kt
@@ -0,0 +1,28 @@
+package com.woowahan.ordering.ui.adapter.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.woowahan.ordering.databinding.ItemFoodGridBinding
+import com.woowahan.ordering.domain.model.Food
+import com.woowahan.ordering.ui.adapter.foodDiffUtil
+import com.woowahan.ordering.ui.adapter.viewholder.ItemFoodViewHolder
+
+class FoodGridAdapter: ListAdapter<Food, ItemFoodViewHolder.Grid>(foodDiffUtil) {
+    private var onClick: (String) -> Unit = {}
+    fun setOnClick(onClick: (String) -> Unit) {
+        this.onClick = onClick
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemFoodViewHolder.Grid {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemFoodGridBinding.inflate(inflater, parent, false)
+        return ItemFoodViewHolder.Grid(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemFoodViewHolder.Grid, position: Int) {
+        holder.bind(getItem(position)) {
+            onClick(it)
+        }
+    }
+}
---
+++ presentation/src/main/java/com/woowahan/ordering/ui/adapter/viewholder/ItemFoodViewHolder.kt
@@ -0,0 +1,19 @@
+package com.woowahan.ordering.ui.adapter.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.woowahan.ordering.databinding.ItemFoodGridBinding
+import com.woowahan.ordering.databinding.ItemFoodLinearBinding
+import com.woowahan.ordering.domain.model.Food
+
+sealed class ItemFoodViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class Grid(private val binding: ItemFoodGridBinding): ItemFoodViewHolder(binding.root) {
+        fun bind(food: Food, onClick: (String) -> Unit) {
+            binding.food = food
+            binding.root.setOnClickListener {
+                onClick(food.detailHash)
+            }
+        }
+    }
+
+}
