9c0df54f5e3aa281237ee5d13270a57ae450d5bb
2004yyj <2004yyj@naver.com>
2022-08-11 13:41:18 +0900

    [ADD] Best Adapter와 RecyclerView 연동

--- presentation/src/main/java/com/woowahan/ordering/ui/fragment/home/best/BestFragment.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/fragment/home/best/BestFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ConcatAdapter
 import com.woowahan.ordering.databinding.FragmentBestBinding
+import com.woowahan.ordering.ui.adapter.home.BestFoodAdapter
+import com.woowahan.ordering.ui.adapter.home.HeaderAdapter
 
 class BestFragment(
     private val onDetailClick: (title: String, hash: String) -> Unit
 ) : Fragment() {
 
     private lateinit var binding: FragmentBestBinding
+    private val adapter: BestFoodAdapter by lazy {
+        BestFoodAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +25,22 @@ class BestFragment(
     ): View {
         binding = FragmentBestBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+        initRecyclerView()
+    }
+
+    private fun initListener() {
+        adapter.setOnClick(onDetailClick)
+    }
+
+    private fun initRecyclerView() = with(binding) {
+        val headerAdapter = HeaderAdapter("한 번 주문하면\n두 번 반하는 반찬들", "기획전")
+        rvBest.adapter = ConcatAdapter(headerAdapter, adapter)
     }
 
     companion object {
