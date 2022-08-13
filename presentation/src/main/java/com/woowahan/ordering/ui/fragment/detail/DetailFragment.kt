d397bdf88a0d8b2f49a690446907be183803335b
2004yyj <2004yyj@naver.com>
2022-08-13 18:01:12 +0900

    [ADD] RecyclerView

--- presentation/src/main/java/com/woowahan/ordering/ui/fragment/detail/DetailFragment.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/fragment/detail/DetailFragment.kt
@@ -5,14 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ConcatAdapter
 import com.woowahan.ordering.R
+import com.woowahan.ordering.databinding.FragmentDetailBinding
+import com.woowahan.ordering.ui.adapter.detail.DetailThumbImagesAdapter
+import com.woowahan.ordering.ui.viewmodel.DetailViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
+
+    private var binding: FragmentDetailBinding? = null
+    private val detailImagesAdapter by lazy { DetailThumbImagesAdapter() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(inflater)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initArguments()
+        initRecyclerView()
+        viewModel.getFoodDetail(hash)
+    }
+
+    private fun initArguments() = with(requireArguments()) {
+        hash = getString("hash", "")
+        title = getString("title", "")
+    }
+
+    private fun initRecyclerView() = with(binding!!) {
+        val adapter = ConcatAdapter(detailImagesAdapter)
+        rvDetail.adapter = adapter
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
