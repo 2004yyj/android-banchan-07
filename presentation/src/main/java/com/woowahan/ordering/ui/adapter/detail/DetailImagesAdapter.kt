71eb0fcdf5d9418f17fbb9336110b6499e24f892
2004yyj <2004yyj@naver.com>
2022-08-13 17:31:01 +0900

    [ADD] Detail Images Adapter

---
+++ presentation/src/main/java/com/woowahan/ordering/ui/adapter/detail/DetailImagesAdapter.kt
@@ -0,0 +1,33 @@
+package com.woowahan.ordering.ui.adapter.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabLayoutMediator
+import com.woowahan.ordering.databinding.ItemDetailImagesBinding
+
+class DetailImagesAdapter(private val thumbImages: List<String>) :
+    RecyclerView.Adapter<DetailImagesAdapter.DetailImagesViewHolder>() {
+    inner class DetailImagesViewHolder(
+        private val binding: ItemDetailImagesBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind() = with(binding) {
+            val adapter = ImagesViewPagerAdapter()
+            vpImages.adapter = adapter
+            TabLayoutMediator(tbBottomDots, vpImages) { _, _ -> }.attach()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailImagesViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemDetailImagesBinding.inflate(inflater)
+        return DetailImagesViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DetailImagesViewHolder, position: Int) {
+        holder.bind()
+    }
+
+    override fun getItemCount(): Int = 1
+
+}
