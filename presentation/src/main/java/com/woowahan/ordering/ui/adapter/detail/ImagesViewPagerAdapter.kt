51071b0f3fdabb85b1e3d8fb67633835eac8b8a5
2004yyj <2004yyj@naver.com>
2022-08-13 19:20:58 +0900

    [FIX] item_image.xml -> item_image_view_pager.xml

--- presentation/src/main/java/com/woowahan/ordering/ui/adapter/detail/ImagesViewPagerAdapter.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/adapter/detail/ImagesViewPagerAdapter.kt
@@ -5,13 +5,14 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.woowahan.ordering.databinding.ItemImageBinding
+import com.woowahan.ordering.databinding.ItemImageViewPagerBinding
 import com.woowahan.ordering.ui.adapter.stringDiffUtil
 
 class ImagesViewPagerAdapter :
     ListAdapter<String, ImagesViewPagerAdapter.ImagesViewPagerViewHolder>(stringDiffUtil) {
 
     inner class ImagesViewPagerViewHolder(
-        private val binding: ItemImageBinding
+        private val binding: ItemImageViewPagerBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(url: String) {
             binding.image = url
@@ -20,7 +21,7 @@ class ImagesViewPagerAdapter :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImagesViewPagerViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemImageBinding.inflate(inflater, parent, false)
+        val binding = ItemImageViewPagerBinding.inflate(inflater, parent, false)
         return ImagesViewPagerViewHolder(binding)
     }
 
