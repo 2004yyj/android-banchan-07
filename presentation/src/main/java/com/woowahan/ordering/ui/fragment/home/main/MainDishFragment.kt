a78bba9a1d5a17708e35329d38b676fbf779d93f
2004yyj <2004yyj@naver.com>
2022-08-13 15:26:31 +0900

    [FIX] 어댑터 초기화 시 lateinit 사용

--- presentation/src/main/java/com/woowahan/ordering/ui/fragment/home/main/MainDishFragment.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/fragment/home/main/MainDishFragment.kt
@@ -33,7 +33,7 @@ class MainDishFragment: Fragment() {
     private val viewModel by viewModels<MainDishViewModel>()
 
     private val typeAndFilterAdapter by lazy { TypeAndFilterAdapter() }
-    private val foodAdapter by lazy { FoodAdapter() }
+    private lateinit var foodAdapter: FoodAdapter
 
     private val gridDecoration = ItemSpacingDecoratorWithHeader(
         spacing = 18.dp,
@@ -86,6 +86,7 @@ class MainDishFragment: Fragment() {
     }
 
     private fun initRecyclerView() = with(binding!!) {
+        foodAdapter = FoodAdapter()
         val headerAdapter = HeaderAdapter("모두가 좋아하는\n든든한 메인 요리")
         val concatAdapter = ConcatAdapter(headerAdapter, typeAndFilterAdapter, foodAdapter)
 
