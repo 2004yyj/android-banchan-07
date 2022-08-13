b1c98cbcfca81621c28492688ab24e8ce185cd39
2004yyj <2004yyj@naver.com>
2022-08-13 17:30:47 +0900

    [ADD] Image View Pager Adapter

---
+++ presentation/src/main/java/com/woowahan/ordering/ui/adapter/detail/ImagesViewPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.woowahan.ordering.ui.adapter.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.woowahan.ordering.databinding.ItemImageBinding
+import com.woowahan.ordering.ui.adapter.stringDiffUtil
+
+class ImagesViewPagerAdapter :
+    ListAdapter<String, ImagesViewPagerAdapter.ImagesViewPagerViewHolder>(stringDiffUtil) {
+
+    inner class ImagesViewPagerViewHolder(
+        private val binding: ItemImageBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(url: String) {
+            binding.image = url
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImagesViewPagerViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemImageBinding.inflate(inflater)
+        return ImagesViewPagerViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ImagesViewPagerViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
