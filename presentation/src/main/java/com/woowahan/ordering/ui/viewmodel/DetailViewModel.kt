cf9a57b8c933f9b8dc881f3ff5a2b817cfb4984a
2004yyj <2004yyj@naver.com>
2022-08-16 00:39:20 +0900

    [ADD] 숫자 Plus Minus, AddToCart 메소드 추가

--- presentation/src/main/java/com/woowahan/ordering/ui/viewmodel/DetailViewModel.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/viewmodel/DetailViewModel.kt
@@ -2,9 +2,12 @@ package com.woowahan.ordering.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.woowahan.ordering.domain.model.Cart
 import com.woowahan.ordering.domain.model.FoodDetail
 import com.woowahan.ordering.domain.model.Result
+import com.woowahan.ordering.domain.usecase.cart.InsertCartUseCase
 import com.woowahan.ordering.domain.usecase.food.GetFoodDetailUseCase
+import com.woowahan.ordering.ui.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -16,17 +19,61 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
-    private val getFoodDetailUseCase: GetFoodDetailUseCase
+    private val getFoodDetailUseCase: GetFoodDetailUseCase,
+    private val insertCartUseCase: InsertCartUseCase
 ): ViewModel() {
+
+    private val _uiState = MutableSharedFlow<UiState>()
+    val uiState = _uiState.asSharedFlow()
 
     private val _foodDetail = MutableStateFlow<FoodDetail?>(null)
     val foodDetail = _foodDetail.asStateFlow()
+
+    private var _count = MutableStateFlow(1)
+    val count = _count.asStateFlow()
+
+    fun increaseCount() {
+        _count.value = _count.value + 1
+    }
+
+    fun decreaseCount() {
+        if (count.value > 1) {
+            _count.value = _count.value - 1
+        }
+    }
 
     fun getFoodDetail(hash: String) {
         viewModelScope.launch(Dispatchers.IO) {
             getFoodDetailUseCase(hash).collect {
                 if (it is Result.Success) {
                     _foodDetail.emit(it.value)
+                }
+            }
+        }
+    }
+
+    fun addToCart(title: String, food: FoodDetail) {
+        val cart = Cart(
+            id = 0,
+            title = title,
+            thumbnail = food.topImage,
+            price = food.discountedPrice,
+            count = _count.value,
+            detailHash = food.hash
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            insertCartUseCase(cart).collect {
+                when (it) {
+                    is Result.Loading -> {
+                        _uiState.emit(UiState.Loading)
+                    }
+                    is Result.Success -> {
+                        _uiState.emit(UiState.Success)
+                    }
+                    is Result.Failure -> {
+                        _uiState.emit(UiState.Error(it.cause.toString()))
+                    }
                 }
             }
         }
