b1d82934f67738a96e785ad8863eeec79037f3b9
Jihye Joo <oreocube13@gmail.com>
2022-08-15 23:30:39 +0900

    [FEAT] #42 - 타임스탬프 익스텐션, 바인딩 어댑터 구현

---
+++ presentation/src/main/java/com/woowahan/ordering/util/DateUtil.kt
@@ -0,0 +1,18 @@
+package com.woowahan.ordering.util
+
+fun Long.getDiffFromNow(): String {
+    val now = System.currentTimeMillis()
+    val diff = now - this
+
+    val seconds = diff / 1000
+    val minutes = seconds / 60
+    val hours = minutes / 60
+    val days = hours / 24
+
+    if (days >= 365) return "${days / 365}년전"
+    if (days >= 30) return "${days / 30}달전"
+    if (days > 0) return "${days}일전"
+    if (hours > 0) return "${hours}시간전"
+    if (minutes > 0) return "${minutes}분전"
+    return "${seconds}초전"
+}
