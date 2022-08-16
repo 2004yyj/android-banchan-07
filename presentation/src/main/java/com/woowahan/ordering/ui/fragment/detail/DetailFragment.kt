a74e8e09e67773b0d61ae65f42580804e387d73a
2004yyj <2004yyj@naver.com>
2022-08-16 17:15:31 +0900

    [ADD] Recently에 Detail 추가하기

--- presentation/src/main/java/com/woowahan/ordering/ui/fragment/detail/DetailFragment.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/fragment/detail/DetailFragment.kt
@@ -62,6 +62,7 @@ class DetailFragment : Fragment() {
                 it?.let {
                     detailThumbImagesAdapter.submitList(it.thumbImages)
                     detailImagesFooterAdapter.submitList(it.detailSection)
+                    viewModel.insertRecently(title, it)
                 }
             }
         }
