188adba40d06247102c401652bbe2f03b6cafb0f
2004yyj <2004yyj@naver.com>
2022-08-17 11:04:02 +0900

    [ADD] SimpleOrder DB와 데이터 연결
    Co-Authored-By: 주지혜 <oreocube13@gmail.com>

--- presentation/src/main/java/com/woowahan/ordering/ui/fragment/order/OrderListFragment.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/fragment/order/OrderListFragment.kt
@@ -5,11 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.woowahan.ordering.R
 import com.woowahan.ordering.databinding.FragmentOrderListBinding
+import com.woowahan.ordering.ui.adapter.order.SimpleOrderListAdapter
+import com.woowahan.ordering.ui.decorator.ItemSpacingDecoratorWithHeader
+import com.woowahan.ordering.ui.decorator.ItemSpacingDecoratorWithHeader.Companion.VERTICAL
+import com.woowahan.ordering.ui.viewmodel.OrderListViewModel
+import com.woowahan.ordering.util.dp
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OrderListFragment : Fragment() {
 
+    private val viewModel: OrderListViewModel by viewModels()
+    private lateinit var adapter: SimpleOrderListAdapter
     private var binding: FragmentOrderListBinding? = null
 
     override fun onCreateView(
@@ -23,6 +34,28 @@ class OrderListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initFlow()
+        initRecyclerView()
+
+        viewModel.getSimpleOrder()
+    }
+
+    private fun initFlow() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.simpleOrder.collect {
+                adapter.submitList(it)
+            }
+        }
+    }
+
+    private fun initRecyclerView() = with(binding!!) {
+        adapter = SimpleOrderListAdapter()
+        rvOrderList.adapter = adapter
+        rvOrderList.addItemDecoration(ItemSpacingDecoratorWithHeader(
+            spacing = 9.dp,
+            removeSpacePosition = listOf(),
+            layoutDirection = VERTICAL
+        ))
     }
 
     override fun onDestroyView() {
---
+++ presentation/src/main/java/com/woowahan/ordering/ui/viewmodel/OrderListViewModel.kt
@@ -0,0 +1,29 @@
+package com.woowahan.ordering.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.woowahan.ordering.domain.model.SimpleOrder
+import com.woowahan.ordering.domain.usecase.order.GetSimpleOrderUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OrderListViewModel @Inject constructor(
+    private val simpleOrderUseCase: GetSimpleOrderUseCase
+) : ViewModel() {
+
+    private val _simpleOrder = MutableStateFlow<List<SimpleOrder>>(emptyList())
+    val simpleOrder = _simpleOrder.asStateFlow()
+
+    fun getSimpleOrder() {
+        viewModelScope.launch(Dispatchers.IO) {
+            simpleOrderUseCase().collect {
+                _simpleOrder.emit(it)
+            }
+        }
+    }
+}
