280c5ed58e08c234f844102093f8e520282b2f42
2004yyj <2004yyj@naver.com>
2022-08-17 22:33:39 +0900

    [ADD] MainActivity에 Install하기
    Co-Authored-By: 주지혜 <oreocube13@gmail.com>

--- presentation/src/main/java/com/woowahan/ordering/ui/activity/MainActivity.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.woowahan.ordering.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
@@ -30,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         cartBinding = ActionCartBinding.inflate(layoutInflater)
         orderBinding = ActionOrderBinding.inflate(layoutInflater)
