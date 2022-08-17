1b7639a9a28e99bd5b25f294bb8f6753d6730f7c
Jihye Joo <oreocube13@gmail.com>
2022-08-17 11:30:05 +0900

    [FEAT] #66 - 최근 본 상품 조회 화면 구현

--- data/src/main/java/com/woowahan/ordering/data/mapper/RecentlyMapper.kt
+++ data/src/main/java/com/woowahan/ordering/data/mapper/RecentlyMapper.kt
@@ -1,6 +1,8 @@
 package com.woowahan.ordering.data.mapper
 
 import com.woowahan.ordering.data.entity.RecentlyEntity
+import com.woowahan.ordering.domain.model.Cart
+import com.woowahan.ordering.domain.model.Food
 import com.woowahan.ordering.domain.model.Recently
 
 fun Recently.toEntity(): RecentlyEntity {
@@ -22,5 +24,32 @@ fun RecentlyEntity.toModel(): Recently {
         price,
         discountPrice,
         latestViewedTime
+    )
+}
+
+fun Recently.toCartModel(): Cart {
+    return Cart(
+        id = 0,
+        title = title,
+        thumbnail = thumbnail,
+        price = discountedPrice,
+        count = 1,
+        detailHash = detailHash
+    )
+}
+
+fun Recently.toFoodModel(): Food {
+    return Food(
+        detailHash = detailHash,
+        image = thumbnail,
+        alt = "",
+        deliveryType = listOf(),
+        title = title,
+        description = "",
+        price = price,
+        discountedPrice = discountedPrice,
+        discountRate = (if (price == 0L) 0 else (1 - (discountedPrice.toFloat() / price.toFloat())) * 100).toInt(),
+        badge = listOf(),
+        isAdded = isAdded
     )
 }
---
+++ presentation/src/main/java/com/woowahan/ordering/ui/fragment/cart/recently/RecentlyViewedFragment.kt
@@ -0,0 +1,92 @@
+package com.woowahan.ordering.ui.fragment.cart.recently
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.woowahan.ordering.databinding.FragmentRecentlyViewedBinding
+import com.woowahan.ordering.domain.model.Food
+import com.woowahan.ordering.ui.adapter.cart.RecentlyAdapter
+import com.woowahan.ordering.ui.decorator.ItemSpacingDecoratorWithHeader
+import com.woowahan.ordering.ui.decorator.ItemSpacingDecoratorWithHeader.Companion.GRID
+import com.woowahan.ordering.ui.dialog.CartBottomSheet
+import com.woowahan.ordering.ui.dialog.CartDialogFragment
+import com.woowahan.ordering.ui.dialog.IsExistsCartDialogFragment
+import com.woowahan.ordering.ui.viewmodel.RecentlyViewedViewModel
+import com.woowahan.ordering.util.dp
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RecentlyViewedFragment : Fragment() {
+
+    private var binding: FragmentRecentlyViewedBinding? = null
+    private val viewModel by viewModels<RecentlyViewedViewModel>()
+    private lateinit var adapter: RecentlyAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRecentlyViewedBinding.inflate(layoutInflater)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+        initFlow()
+    }
+
+    private fun initRecyclerView() = with(binding!!) {
+        adapter = RecentlyAdapter(false) {
+            showCartBottomSheet(it)
+        }
+        rvRecently.adapter = adapter
+        rvRecently.addItemDecoration(
+            ItemSpacingDecoratorWithHeader(
+                spacing = 16.dp,
+                layoutDirection = GRID
+            )
+        )
+    }
+
+    private fun initFlow() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.recentlyViewedList.collect {
+                adapter.submitList(it)
+            }
+        }
+    }
+
+    private fun showCartBottomSheet(food: Food) {
+        if (food.isAdded) {
+            IsExistsCartDialogFragment.newInstance {
+                navigateToCart()
+            }.show(parentFragmentManager, tag)
+        } else {
+            CartBottomSheet.newInstance(food) {
+                showCartDialog()
+            }.show(parentFragmentManager, tag)
+        }
+    }
+
+    private fun showCartDialog() {
+        CartDialogFragment.newInstance {
+            navigateToCart()
+        }.show(parentFragmentManager, tag)
+    }
+
+    private fun navigateToCart() {
+        // TODO
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
