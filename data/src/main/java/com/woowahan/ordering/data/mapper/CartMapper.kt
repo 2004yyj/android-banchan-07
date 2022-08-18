03b9885473ab2d86eebc2ad22748bc37c7a759be
2004yyj <2004yyj@naver.com>
2022-08-18 14:17:07 +0900

    [FIX] cart result title 가격이 가장 큰 음식 제목으로 설정

--- data/src/main/java/com/woowahan/ordering/data/mapper/CartMapper.kt
+++ data/src/main/java/com/woowahan/ordering/data/mapper/CartMapper.kt
@@ -36,10 +36,13 @@ fun List<CartEntity>.toCartResult(): CartResult{
     val list = map { cart -> cart.toModel() }
     val checkedList = list.filter { it.isChecked }
     val sum = checkedList.sumOf { it.price * it.count }
-    val count = list.size
+    val maxPriceTitle =
+        if (list.isNotEmpty())
+            list.maxByOrNull { it.price }!!.title
+        else ""
 
     return CartResult(
-        title = if (list.isNotEmpty()) list[0].title else "",
+        title = maxPriceTitle,
         count = list.size,
         isSelectedAll = checkedList.size == this.size,
         list = list,
