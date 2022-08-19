56b4d9dad7ad08bfd2c1ef1b2956e814e26015f0
2004yyj <2004yyj@naver.com>
2022-08-19 20:00:37 +0900

    [ADD] PendingIntent로 MainActivity 이동

--- presentation/src/main/java/com/woowahan/ordering/ui/receiver/CartReceiver.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/receiver/CartReceiver.kt
@@ -2,11 +2,15 @@ package com.woowahan.ordering.ui.receiver
 
 import android.app.Notification
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.core.content.ContextCompat
 import com.woowahan.ordering.R
+import com.woowahan.ordering.ui.activity.MainActivity
 import com.woowahan.ordering.util.createChannel
 import com.woowahan.ordering.util.executeRandom
 
@@ -14,6 +18,7 @@ class CartReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val food = intent.getStringExtra(FOOD_TITLE)
         val foodCount = intent.getIntExtra(FOOD_COUNT, 0) - 1
+        val deliveryTime = intent.getLongExtra(DELIVERY_FINISHED_TIME, 0L)
         val content =
             if (foodCount > 0)
                 context.getString(R.string.cart_notification_content_with_count, food, foodCount)
@@ -24,6 +29,15 @@ class CartReceiver: BroadcastReceiver() {
         val channelName = context.getString(R.string.cart_channel_name)
 
         val notificationManager = context.getSystemService(NotificationManager::class.java)
+        val mainIntent = Intent(context, MainActivity::class.java).apply {
+            putExtra(DELIVERY_FINISHED_TIME, deliveryTime)
+        }
+
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            getActivity(context, executeRandom(), mainIntent, FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT)
+        } else {
+            getActivity(context, executeRandom(), mainIntent, FLAG_UPDATE_CURRENT)
+        }
         notificationManager.createChannel(channelId, channelName)
         val notification =
             Notification.Builder(context, channelId)
@@ -32,6 +46,7 @@ class CartReceiver: BroadcastReceiver() {
                 .setContentTitle(context.getString(R.string.delivery_success))
                 .setContentText(content)
                 .setStyle(Notification.BigTextStyle().bigText(content))
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
                 .build()
         notificationManager.notify(executeRandom(), notification)
