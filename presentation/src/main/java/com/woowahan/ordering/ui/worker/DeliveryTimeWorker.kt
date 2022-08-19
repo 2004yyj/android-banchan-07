cec263f783c6c708d52bd2c07adab32affc31eb3
2004yyj <2004yyj@naver.com>
2022-08-20 00:07:30 +0900

    [ADD] Worker Class

---
+++ presentation/src/main/java/com/woowahan/ordering/ui/worker/DeliveryTimeWorker.kt
@@ -0,0 +1,27 @@
+package com.woowahan.ordering.ui.worker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.woowahan.ordering.domain.usecase.order.UpdateOrderUseCase
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.coroutineScope
+
+@HiltWorker
+class DeliveryTimeWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParameters: WorkerParameters,
+    private val updateOrderUseCase: UpdateOrderUseCase
+): CoroutineWorker(context, workerParameters) {
+    override suspend fun doWork(): Result {
+        coroutineScope {
+            val deliveryTime = inputData.getLong("deliveryTime", 0)
+            updateOrderUseCase(deliveryTime, true)
+        }
+
+        return Result.success()
+    }
+
+}
