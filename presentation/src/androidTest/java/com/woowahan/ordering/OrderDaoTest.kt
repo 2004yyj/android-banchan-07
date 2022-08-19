a75a11663f6c916cd2e69abe6a4c6211cfe411ce
2004yyj <2004yyj@naver.com>
2022-08-19 22:50:11 +0900

    [TEST] UpdateOrder Test 성공

---
+++ presentation/src/androidTest/java/com/woowahan/ordering/OrderDaoTest.kt
@@ -0,0 +1,65 @@
+package com.woowahan.ordering
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.woowahan.ordering.data.entity.CartEntity
+import com.woowahan.ordering.data.entity.OrderEntity
+import com.woowahan.ordering.data.local.dao.CartDao
+import com.woowahan.ordering.data.local.dao.OrderDao
+import com.woowahan.ordering.data.local.db.AppDatabase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class OrderDaoTest {
+
+    private lateinit var db: AppDatabase
+    private lateinit var orderDao: OrderDao
+    private val context: Context = ApplicationProvider.getApplicationContext()
+
+    @Before
+    fun createOrderDao() {
+        db = Room.inMemoryDatabaseBuilder(
+            context, AppDatabase::class.java
+        ).build()
+        orderDao = db.orderDao()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun orderInsert() = runTest {
+        val orderEntity = OrderEntity(0, System.currentTimeMillis(), false)
+        try {
+            orderDao.insertOrder(orderEntity)
+        } catch (e: IOException) {
+            assertThat(e.cause, equalTo(null))
+        }
+    }
+
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun updateOrder_test() = runTest {
+        val orderEntity = OrderEntity(1, System.currentTimeMillis(), false)
+        try {
+            orderDao.updateOrder(orderEntity)
+        } catch (e: IOException) {
+            assertThat(e.cause, equalTo(null))
+        }
+    }
+
+    @After
+    fun closeDB() {
+        db.close()
+    }
+}
