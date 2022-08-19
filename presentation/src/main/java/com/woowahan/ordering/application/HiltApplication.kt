e114821f13b1bc98d5d92742430e1be01ba374ab
2004yyj <2004yyj@naver.com>
2022-08-19 17:50:22 +0900

    [FIX] Application에서 Notification Channel 생성하는 코드 제거

--- presentation/src/main/java/com/woowahan/ordering/application/HiltApplication.kt
+++ presentation/src/main/java/com/woowahan/ordering/application/HiltApplication.kt
@@ -1,17 +1,7 @@
 package com.woowahan.ordering.application
 
 import android.app.Application
-import android.app.NotificationManager
-import com.woowahan.ordering.util.NotificationUtil.CART_CHANNEL_ID
-import com.woowahan.ordering.util.NotificationUtil.CART_CHANNEL_NAME
-import com.woowahan.ordering.util.createChannel
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HiltApplication: Application() {
-    override fun onCreate() {
-        super.onCreate()
-        val notificationManager = getSystemService(NotificationManager::class.java)
-        notificationManager.createChannel(CART_CHANNEL_ID, CART_CHANNEL_NAME)
-    }
-}
+class HiltApplication: Application()
