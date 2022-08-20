3257130d527ab962b57cb2f6c87b64ed45fa9c0b
Jihye Joo <oreocube13@gmail.com>
2022-08-20 13:00:42 +0900

    [CHORE] #125 - 불필요한 매퍼 제거

--- data/src/main/java/com/woowahan/ordering/data/mapper/RecentlyMapper.kt
+++ data/src/main/java/com/woowahan/ordering/data/mapper/RecentlyMapper.kt
@@ -3,7 +3,6 @@ package com.woowahan.ordering.data.mapper
 import com.woowahan.ordering.data.entity.RecentlyEntity
 import com.woowahan.ordering.data.util.getDiscountRate
 import com.woowahan.ordering.domain.model.Cart
-import com.woowahan.ordering.domain.model.Food
 import com.woowahan.ordering.domain.model.Recently
 
 fun Recently.toEntity(): RecentlyEntity {
@@ -36,21 +35,5 @@ fun Recently.toCartModel(): Cart {
         discountedPrice = discountedPrice,
         originalPrice = price,
         detailHash = detailHash
-    )
-}
-
-fun Recently.toFoodModel(): Food {
-    return Food(
-        detailHash = detailHash,
-        image = thumbnail,
-        alt = "",
-        deliveryType = listOf(),
-        title = title,
-        description = "",
-        price = price,
-        discountedPrice = discountedPrice,
-        discountRate = (if (price == 0L) 0 else (1 - (discountedPrice.toFloat() / price.toFloat())) * 100).toInt(),
-        badge = listOf(),
-        isAdded = isAdded
     )
 }
