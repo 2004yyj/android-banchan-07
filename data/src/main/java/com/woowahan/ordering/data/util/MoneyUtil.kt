d51d77dbe0c491f4ad2d903fd8fee627b5247e19
Jihye Joo <oreocube13@gmail.com>
2022-08-20 11:26:48 +0900

    [ADD] #125 - 할인율 계산 확장 함수 추가

--- data/src/main/java/com/woowahan/ordering/data/util/MoneyUtil.kt
+++ data/src/main/java/com/woowahan/ordering/data/util/MoneyUtil.kt
@@ -3,3 +3,8 @@ package com.woowahan.ordering.data.util
 fun String.toMoneyLong(): Long {
     return this.replace("[^0-9]".toRegex(), "").toLong()
 }
+
+fun Long.getDiscountRate(original: Long): Int {
+    return if (original == 0L) 0
+    else ((1 - (this.toFloat() / original.toFloat())) * 100).toInt()
+}
