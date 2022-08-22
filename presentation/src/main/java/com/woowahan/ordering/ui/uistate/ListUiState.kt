0e03a0208be2927773257ec8f9f453b84e01ca63
2004yyj <2004yyj@naver.com>
2022-08-22 22:33:26 +0900

    [ADD] ListUiState

---
+++ presentation/src/main/java/com/woowahan/ordering/ui/uistate/ListUiState.kt
@@ -0,0 +1,6 @@
+package com.woowahan.ordering.ui.uistate
+
+sealed class ListUiState<out S> {
+    object Refreshing : ListUiState<Nothing>()
+    class List<out S>(val list: kotlin.collections.List<S> = emptyList()) : ListUiState<S>()
+}
