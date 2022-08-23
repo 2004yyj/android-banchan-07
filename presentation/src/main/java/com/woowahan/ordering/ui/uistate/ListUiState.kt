1324eda04287b0b5a9a35de649e81a9168c3b214
2004yyj <2004yyj@naver.com>
2022-08-23 12:49:21 +0900

    [ADD] No Internet ListUiState

--- presentation/src/main/java/com/woowahan/ordering/ui/uistate/ListUiState.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/uistate/ListUiState.kt
@@ -3,4 +3,5 @@ package com.woowahan.ordering.ui.uistate
 sealed class ListUiState<out S> {
     object Refreshing : ListUiState<Nothing>()
     class List<out S>(val list: kotlin.collections.List<S> = emptyList()) : ListUiState<S>()
+    object NoInternet : ListUiState<Nothing>()
 }
