cef5f69439baf8141e58d12e59fd00bbd9ddf310
2004yyj <2004yyj@naver.com>
2022-08-24 15:24:41 +0900

    [REFACTOR] #176 - Notification 생성하는 클래스 나누기

--- presentation/src/main/java/com/woowahan/ordering/ui/receiver/CartReceiver.kt
+++ presentation/src/main/java/com/woowahan/ordering/ui/receiver/CartReceiver.kt
@@ -1,30 +1,27 @@
 package com.woowahan.ordering.ui.receiver
 
-import android.app.Notification
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.app.PendingIntent.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import androidx.core.content.ContextCompat
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.woowahan.ordering.R
 import com.woowahan.ordering.ui.worker.DeliveryTimeWorker
 import com.woowahan.ordering.ui.activity.MainActivity
-import com.woowahan.ordering.util.createChannel
+import com.woowahan.ordering.util.NotificationUtil
 import com.woowahan.ordering.util.executeRandom
 
-class CartReceiver: BroadcastReceiver() {
+class CartReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val food = intent.getStringExtra(FOOD_TITLE)
         val foodCount = intent.getIntExtra(FOOD_COUNT, 0) - 1
         val deliveryTime = intent.getLongExtra(DELIVERY_FINISHED_TIME, 0L)
         callWorker(context, deliveryTime)
-        
+
+        val title = context.getString(R.string.delivery_success)
         val content =
             if (foodCount > 0)
                 context.getString(R.string.cart_notification_content_with_count, food, foodCount)
@@ -38,23 +35,12 @@ class CartReceiver: BroadcastReceiver() {
         val mainIntent = Intent(context, MainActivity::class.java).apply {
             putExtra(DELIVERY_FINISHED_TIME, deliveryTime)
         }
-
-        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val pendingIntent =
             getActivity(context, executeRandom(), mainIntent, FLAG_IMMUTABLE or FLAG_UPDATE_CURRENT)
-        } else {
-            getActivity(context, executeRandom(), mainIntent, FLAG_UPDATE_CURRENT)
-        }
-        notificationManager.createChannel(channelId, channelName)
-        val notification =
-            Notification.Builder(context, channelId)
-                .setSmallIcon(R.drawable.ic_notification)
-                .setColor(ContextCompat.getColor(context, R.color.primary_main))
-                .setContentTitle(context.getString(R.string.delivery_success))
-                .setContentText(content)
-                .setStyle(Notification.BigTextStyle().bigText(content))
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-                .build()
+        val notificationUtil =
+            NotificationUtil(notificationManager, context, channelId, channelName)
+        notificationUtil.createChannel()
+        val notification = notificationUtil.createNotification(title, content, pendingIntent)
         notificationManager.notify(executeRandom(), notification)
     }
 
