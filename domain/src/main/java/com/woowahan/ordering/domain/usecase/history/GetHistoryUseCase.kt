4b21c75daa0a9af0773e0bb2f7683fd9e52f7825
Jihye Joo <oreocube13@gmail.com>
2022-08-24 16:48:18 +0900

    [FIX] #178 - GetHistory 유스케이스 개선

--- domain/src/main/java/com/woowahan/ordering/domain/usecase/history/GetHistoryUseCase.kt
+++ domain/src/main/java/com/woowahan/ordering/domain/usecase/history/GetHistoryUseCase.kt
@@ -1,6 +1,8 @@
 package com.woowahan.ordering.domain.usecase.history
 
+import androidx.paging.PagingData
 import androidx.paging.map
+import com.woowahan.ordering.domain.model.History
 import com.woowahan.ordering.domain.repository.CartRepository
 import com.woowahan.ordering.domain.repository.HistoryRepository
 import kotlinx.coroutines.CoroutineDispatcher
@@ -11,15 +13,13 @@ class GetHistoryUseCase(
     private val cartRepository: CartRepository,
     private val ioDispatcher: CoroutineDispatcher
 ) {
-    operator fun invoke() = flow {
-        historyRepository.getAllHistories()
+    operator fun invoke(): Flow<PagingData<History>> {
+        return historyRepository.getAllHistories()
             .combine(cartRepository.getCart()) { history, cart ->
                 val hashList = cart.map { it.detailHash }
                 history.map {
                     it.copy(isAdded = hashList.contains(it.detailHash))
                 }
-            }.collect {
-                emit(it)
-            }
-    }.flowOn(ioDispatcher)
+            }.flowOn(ioDispatcher)
+    }
 }
