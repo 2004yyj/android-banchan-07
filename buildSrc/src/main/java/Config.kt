6fa90e03126b482cd583f9e3c6ade998e6264639
양윤재 <2004yyj@naver.com>
2022-08-30 15:06:35 +0900

    [FIX] Version Change

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -4,7 +4,7 @@ object Config {
     const val minSdk = 27
     const val targetSdk = 32
     const val versionCode = 1
-    const val versionName = "1.1"
+    const val versionName = "1.1.1"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val jvmTarget = "1.8"
 }
