fac63d039bd646307a235013306e44a9e785a3d3
2004yyj <2004yyj@naver.com>
2022-08-30 15:01:29 +0900

    [ADD] Config, Dependencies

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,10 @@
+object Config {
+    const val compileSdk = 32
+    const val applicationId = "com.woowahan.ordering"
+    const val minSdk = 27
+    const val targetSdk = 32
+    const val versionCode = 1
+    const val versionName = "1.1"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    const val jvmTarget = "1.8"
+}
